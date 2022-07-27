d83b8347255ad0dde4da25f009c33b2dc5277cd4
Sarvar <numonjonov03@codeschool.uz>
2022-07-27 07:38:07 +0500

    uyga vazifa

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,10 +1,3 @@
-#Create a variable x and assign it the value of 10.
-
-#Create a variable y and assign it the value of 5.
-
-#Create a variable result and assign it the value of x + y.
-
-#Print the value of result.
 x=10
 y=5
 result = x + y
